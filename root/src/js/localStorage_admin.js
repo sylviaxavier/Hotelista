94ab44b929628b0786f708ec4622c864b505ec42
Leonardo Franco <leo55866@gmail.com>
2022-11-14 13:14:38 -0300

    alterações código JS

--- root/src/js/localStorage_admin.js
+++ root/src/js/localStorage_admin.js
@@ -1,20 +1,51 @@
-console.log ('localStoage')
+let codigo = document.querySelector ("#codigo")
+let codigoLabel = document.querySelector ("#codigoLabel")
+let validCodigo = false
 
-const botao = document.querySelector ("#botao")
-const codigo = document.querySelector ("#codigo")
-const nome = document.querySelector ("#nome")
-const tempo = document.querySelector ("#tempo")
-const atividade = document.querySelector ("#atividade")
-const ferias = document.querySelector ("#ferias")
-const afastamento = document.querySelector ("#afastamento")
-const comentarios = document.querySelector ("#comentarios")
-botao.onclick=function (){
 
-    localStorage.setItem('code', codigo.value)
-    localStorage.setItem('name', nome.value)
-    localStorage.setItem('time', tempo.value)
-    localStorage.setItem('activit', atividade.value)
-    localStorage.setItem('vacation', ferias.value)
-    localStorage.setItem('getout', afastamento.value)
-    localStorage.setItem('comments', comentarios.value)
+let nome = document.querySelector ("#nome")
+let nomeLabel = document.querySelector ("#nomeLabel")
+let validNome = false
+
+let tempo = document.querySelector ("#tempo")
+let tempoLabel = document.querySelector ("#tempoLabel")
+let validTempo = false
+
+
+let atividade = document.querySelector ("#atividade")
+let validAtividade = false
+
+let ferias = document.querySelector ("#ferias")
+let validFerias = false
+
+let afastamento = document.querySelector ("#afastamento")
+let validAfastamento = false
+
+let comentarios = document.querySelector ("#comentarios")
+let validComentarios = false
+
+let botao = document.querySelector ("#botao")
+
+codigo.addEventListener('keyup', () => {
+    if(codigo.value.length <=4){
+        codigoLabel.setAttribute('style', 'color: red')
+    } else {
+        codigoLabel.setAttribute('(style)', 'color: green')
+    }
+})
+nome.addEventListener('keyup', () => {
+    if(nome.value.length <=10){
+        nomeLabel.setAttribute('style', 'color: red')
+    } else {
+        nomeLabel.setAttribute('style', 'color: green')
+    }
+})
+
+
+function cadastrar(){
+    if (validCodigo && validNome && validTempo && validAtividade && validAfastamento && validComentarios) {
+        
+    } else {
+        alert('Há algum campo vazio')
+    }
 }
